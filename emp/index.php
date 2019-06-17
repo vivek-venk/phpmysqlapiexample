787a1812bc1faf9d867b0ebdc13b4848dda62770
vivek-venk <vivek.venk@gmail.com>
2019-06-17 22:26:03 +0530

    Index File uploaded for the api/emp

---
+++ emp/index.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+
+<style>
+body {
+  background-color: lightblue;
+}
+</style>
+
+<h1><center><u>HOME Page for the Employee</u</center></h1>
+<br>
+<br>
+<a href="http://localhost/api/emp/Display_Contents.php">Read Employee Database</a>

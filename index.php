19c4a7cdedf361e26dbd1a6db0c0b38a9f71c493
vivek-venk <vivek.venk@gmail.com>
2019-06-25 11:39:19 +0530

    Update

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include('inc/header.php');
 <title>Demo To Create Simple REST API with PHP and MySQL</title>
 <?php include('inc/container.php');?>
 <div class="container">
-	<h2>Simple REST API with PHP and MySQL - Working</h2>	
+	<h2>Simple REST API with PHP and MySQL - 1234</h2>	
 	<form action="" method="get">
 		<div class="form-group">
 			<label for="name">http://localhost/api/emp/read.php(empid)</label>
